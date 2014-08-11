47eb3010b2cc22b24bc6d7459e9a8b403989ed8b
gxwan <gxwan@Ctrip.com>
2014-08-11 15:11:47 +0800

    ok

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.MessagePatterns;
 
 namespace Demo
 {
@@ -10,6 +12,12 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+            ConnectionFactory cf = new ConnectionFactory();
+            var con = cf.CreateConnection();
+            var model = con.CreateModel();
+
+            SimpleRpcClient client = new SimpleRpcClient(model);
+            
         }
     }
 }
